7a303db6753b16ddcdca3c2fdda68c685afe29d1
yalewoo <chongruyuntian@163.com>
2016-01-12 17:23:28 +0800

    add comments

--- Bintree.h
+++ Bintree.h
@@ -7,50 +7,56 @@
 #include <algorithm>
 #include <iostream>
 #include <cstdio>
+
 using std::max;
 using std::cout;
 using std::endl;
 
 #define BinNodePosi(T) BinNode<T>*
 
-#define stature(p) ((p) ? (p)->height : -1)	//height, empty tree has height -1
+#define stature(p) ((p) ? (p)->height : -1)	//该结点的高度 空节点高度为-1
+
+//左孩子的水平位置 用于display
 #define posilchild(p) ((p)->lchild ? (p)->lchild->horizontal_position : (p)->horizontal_position)
 #define posirchild(p) ((p)->rchild ? (p)->rchild->horizontal_position : (p)->horizontal_position)
 
-
-
+//二叉树结点数据结构
 template <typename T>
 class BinNode
 {
 public:
-	T data;
-	BinNodePosi(T) parent;
-	int height;
-	BinNodePosi(T) lchild;
-	BinNodePosi(T) rchild;
+	T data;	//该结点具体数据
+	BinNodePosi(T) parent;	//父节点指针
+	BinNodePosi(T) lchild;	//左孩子指针
+	BinNodePosi(T) rchild;	//右孩子指针
 
-	int horizontal_position;	//Inorder travser index used to display the tree. 
-	int distance_to_root;
+	int height;	//记录该结点高度
+
+	int horizontal_position;	//记录中序遍历下该结点的序号，用于display显示
+	int distance_to_root;	//记录到根节点的距离 用于display显示
 	
 
-	int size();	//how many sub tree node(include this)
+	int size();	//计算以该结点为根的二叉树结点个数（递归算法）
 
-	BinNodePosi(T) insertAsLC(T const &);
-	BinNodePosi(T) insertAsRC(T const &);
-	BinNodePosi(T) succ();
+	BinNodePosi(T) insertAsLC(T const &);	//插入左孩子
+	BinNodePosi(T) insertAsRC(T const &);	//插入右孩子
+	BinNodePosi(T) succ();	//返回中序遍历时该结点的后继指针
+
+	//遍历算法
+	template <typename VST> void travPre_R(VST &);	//先序遍历递归算法
+	template <typename VST> void travPre_I(VST &);	//先序遍历基于栈的算法1
+	template <typename VST> void travPre_II(VST &);	//先序遍历基于栈的算法2
+
+	template <typename VST> void travIn_R(VST &);	//中序遍历递归算法
+	template <typename VST> void travIn_I(VST &);	//中序遍历基于栈的算法
+
+	template <typename VST> void travPost_R(VST &);	//后序遍历递归算法
+	template <typename VST> void travPost_I(VST &);	//后序遍历基于栈的算法
+
+	template <typename VST> void travLevel(VST &);	//层次遍历 基于队列
 
 	
-	template <typename VST> void travPre_R(VST &);
-	template <typename VST> void travPre_I(VST &);
-	template <typename VST> void travPre_II(VST &);
-	template <typename VST> void travIn_R(VST &);
-	template <typename VST> void travIn_I(VST &);
-	template <typename VST> void travPost_R(VST &);
-	template <typename VST> void travPost_I(VST &);
-	template <typename VST> void travLevel(VST &);
-
-	
-	BinNode(T e, BinNodePosi(T) p);
+	BinNode(T e, BinNodePosi(T) p);	//构造二叉树，父节点p，节点内容e
 	BinNode();
 	virtual ~BinNode(){}
 };
@@ -72,9 +78,13 @@ BinNode<T>::BinNode() : data(0), parent(0)
 	rchild = 0;
 	horizontal_position = 0;
 	distance_to_root = 0;
-
 }
 
+/*
+返回该结点为根的子树的结点个数
+递归求解 return lc->size() + rc->size() + 1
+叶子节点 return 1
+*/
 template <typename T>
 int BinNode<T>::size()
 {
@@ -83,8 +93,6 @@ int BinNode<T>::size()
 	if (rchild) s += rchild->size();
 	return s;
 }
-
-
 
 
 template <typename T>
@@ -100,46 +108,55 @@ BinNodePosi(T) BinNode<T>::insertAsRC(T const & e)
 
 
 
-
+//二叉树数据结构
 template <typename T> class BinTree
 {
 protected:
-	int _size;
-	BinNodePosi(T) _root;
+	int _size;	//记录该二叉树结点个数
+	BinNodePosi(T) _root;	//二叉树树根节点指针
 
-	int updateHeight(BinNodePosi(T) x); //update height of x
-	void updateHeightAbove(BinNodePosi(T) x); //update height of x and x's parents
-	void updateDistanceToRoot(BinNodePosi(T) x);
+	int updateHeight(BinNodePosi(T) x); //更新结点x的高度
+	void updateHeightAbove(BinNodePosi(T) x); //更新x以及x的所有祖先元素的高度
+	void updateDistanceToRoot(BinNodePosi(T) x);	//更新x结点距离根节点的距离
 public:
 	BinTree(BinNodePosi(T) root);
 	BinTree();
 	virtual ~BinTree();
-	int size() const { return _size; }	//how many binnodes
-	int height() const { return _root ? _root->height : 0; }
-	bool empty() const { return !_root; }
-	BinNodePosi(T) root() const { return _root; }
-	BinNodePosi(T) insertAsLC(BinNodePosi(T) x, T const & e);
-	BinNodePosi(T) insertAsRC(BinNodePosi(T) x, T const & e);
-	BinNodePosi(T) attachAsLC(BinNodePosi(T) x, BinTree<T>* & S);
-	BinNodePosi(T) attachAsRC(BinNodePosi(T) x, BinTree<T>* & S);
+
+	int size() const { return _size; }	//返回树中结点个数
+	int height() const { return _root ? _root->height : 0; }	//返回树的高度
+	bool empty() const { return !_root; }	//判断树是否为空树
+	BinNodePosi(T) root() const { return _root; }	//返回根节点指针
+
+	//拓扑连接
+	BinNodePosi(T) insertAsLC(BinNodePosi(T) x, T const & e);	//把e作为x的左孩子插入
+	BinNodePosi(T) insertAsRC(BinNodePosi(T) x, T const & e);	//把e作为x的右孩子插入
+	BinNodePosi(T) attachAsLC(BinNodePosi(T) x, BinTree<T>* & S);	//把S作为x的左子树插入
+	BinNodePosi(T) attachAsRC(BinNodePosi(T) x, BinTree<T>* & S);	//把S作为x的右子树插入
+
+	//删除x结点
 	virtual int remove(BinNodePosi(T) x);
+
+	//将以x为根的子树从树中删去
 	BinNodePosi(T) secede(BinNodePosi(T) x);
 
-	template <typename VST> void travPre(VST &v) { _root->travPre_I(v); }
-	template <typename VST> void travIn(VST &v) { _root->travIn_I(v); }
-	template <typename VST> void travPost(VST &v) { _root->travPost_I(v); }
-	template <typename VST> void travLevel(VST &v) { _root->travLevel_I(v); }
+	//树的遍历算法
+	template <typename VST> void travPre(VST &v) { _root->travPre_I(v); }	//先序遍历 对每个节点执行v
+	template <typename VST> void travIn(VST &v) { _root->travIn_I(v); }	//中序遍历
+	template <typename VST> void travPost(VST &v) { _root->travPost_I(v); }	//后序遍历
+	template <typename VST> void travLevel(VST &v) { _root->travLevel_I(v); }	//层次遍历
 
+	//命令行直观显示二叉树
 	void display();
-
 };
+
+
 template <typename T>
 BinTree<T>::BinTree()
 {
 	_size = 0;
 	_root = NULL;
 }
-
 template <typename T>
 BinTree<T>::BinTree(BinNodePosi(T) root) : _root(root)
 {
@@ -152,11 +169,14 @@ BinTree<T>::~BinTree()
 	removeTreeByRootNode(_root);
 }
 
+
+//更新x结点的高度 
 template <typename T>
 int BinTree<T>::updateHeight(BinNodePosi(T) x)
 {
 	return x->height = 1 + max(stature(x->lchild), stature(x->rchild));
 }
+//更新x及其祖先结点高度
 template <typename T>
 void BinTree<T>::updateHeightAbove(BinNodePosi(T) x)
 {
@@ -167,6 +187,7 @@ void BinTree<T>::updateHeightAbove(BinNodePosi(T) x)
 	}
 }
 
+//拓扑连接 步骤为更新size 插入 更新高度
 template <typename T>
 BinNodePosi(T) BinTree<T>::insertAsLC(BinNodePosi(T) x, T const & e)
 {
@@ -218,6 +239,7 @@ BinNodePosi(T) BinTree<T>::attachAsRC(BinNodePosi(T) x, BinTree<T>* & S)
 	return x;
 }
 
+//删除结点
 template <typename T>
 int BinTree<T>::remove(BinNodePosi(T) x)
 {
@@ -230,6 +252,7 @@ int BinTree<T>::remove(BinNodePosi(T) x)
 	_size -= n;
 	return n;
 }
+//分离子树 返回子树的树根
 template <typename T>
 BinNodePosi(T) BinTree<T>::secede(BinNodePosi(T) x)
 {
@@ -248,6 +271,7 @@ BinNodePosi(T) BinTree<T>::secede(BinNodePosi(T) x)
 	return S;
 }
 
+//删除子树 释放空间
 template <typename T>
 int removeTreeByRootNode(BinNodePosi(T) x)
 {
@@ -400,6 +424,7 @@ void BinNode<T>::travLevel(VST & visit)
 	}
 }
 
+//返回中序遍历下的后继指针
 template <typename T>
 BinNodePosi(T) BinNode<T>::succ()
 {
@@ -421,7 +446,7 @@ BinNodePosi(T) BinNode<T>::succ()
 }
 
 
-
+//更新x到root的距离 用于display
 template <typename T>
 void BinTree<T>::updateDistanceToRoot(BinNodePosi(T) x)
 {
@@ -447,66 +472,7 @@ void BinTree<T>::updateDistanceToRoot(BinNodePosi(T) x)
 		if (x->lchild) q.enqueue(x->lchild);
 		if (x->rchild) q.enqueue(x->rchild);
 	}
-
-
-
 }
-
-// template <typename T>
-// void BinTree<T>::display()
-// {
-// 	int count = 0;
-// 	BinNodePosi(T) x = _root;
-// 	while (x->lchild != NULL)
-// 		x = x->lchild;
-
-// 	while (x != NULL)
-// 	{
-// 		x->horizontal_position = ++count;
-// 		x = x->succ();
-// 	}
-
-// 	updateDistanceToRoot(_root);
-
-
-// 	Queue<BinNodePosi(T)> q;
-// 	x = _root;
-	
-// 	q.enqueue(x);
-
-// 	int nowheight = 0;
-// 	int lastheight = 0;
-// 	int levelcount = 0;
-// 	int i = 0;
-// 	while (!q.empty())
-// 	{
-// 		x = q.dequeue();
-
-// 		if (x) 
-// 		{
-// 			nowheight = x->distance_to_root;
-
-// 			if (nowheight != lastheight)
-// 			{
-// 				lastheight = nowheight;
-// 				cout << endl;
-// 				levelcount = 0;
-// 			}
-
-// 			for(i = levelcount; i < x->horizontal_position; ++i)
-// 			{
-// 				printf("\t");
-// 			}
-// 			cout << x->data;
-// 			levelcount += x->horizontal_position - levelcount;
-
-// 		}
-
-// 		if (x->lchild) q.enqueue(x->lchild);
-// 		if (x->rchild) q.enqueue(x->rchild);
-// 	}
-// 	cout << endl << endl;
-// }
 
 
 template <typename T>
